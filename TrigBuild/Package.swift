db9a9263d5c179bdc62873deb7de9b3595fe4271
Tom Woodley <thwoodley@gmail.com>
2023-04-12 22:19:01 +0100

    Trigger Build Package

---
+++ TrigBuild/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TrigBuild",
+    platforms: [
+        .macOS(.v11)
+    ],
+    dependencies: [
+    	.package(url: "https://github.com/AvdLee/appstoreconnect-swift-sdk.git", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "TrigBuild",
+            dependencies: [
+                .product(name: "AppStoreConnect-Swift-SDK", package: "appstoreconnect-swift-sdk")],
+            path: "Sources"),
+    ]
+)
